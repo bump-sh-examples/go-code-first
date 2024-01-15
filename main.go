8c49b86e45a205ab3268ec8ae7b48f16b7830355
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2024-01-15 11:32:50 +0000

    export openapi from cli

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
+// This example shows how to use the CLI to create a server with a command to
+// print the OpenAPI spec. Run it with:
+//
+//	# Print the OpenAPI spec
+//	go run ./examples/spec-cmd openapi
+//
+//	# Save spec to a file
+//	go run ./examples/spec-cmd openapi >spec.yaml
 package main
 
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
-	
+
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	"github.com/go-chi/chi/v5"
+	"github.com/goccy/go-yaml"
+	"github.com/spf13/cobra"
 )
+
+// Options for the CLI.
+type Options struct {
+	Port int `help:"Port to listen on" default:"8888"`
+}
 
 // GreetingInput represents the greeting operation request.
 type GreetingInput struct {
@@ -24,24 +38,43 @@ type GreetingOutput struct {
 }
 
 func main() {
-	// Create a new router & API
-	router := chi.NewMux()
-	api := humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
+	// Store the API so we can access it from other commands later.
+	var api huma.API
 
-	// Register GET /greeting/{name}
-	huma.Register(api, huma.Operation{
-		OperationID: "get-greeting",
-		Summary:     "Get a greeting",
-		Method:      http.MethodGet,
-		Path:        "/greeting/{name}",
-	}, func(ctx context.Context, input *GreetingInput) (*GreetingOutput, error) {
-		resp := &GreetingOutput{}
-		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-		return resp, nil
+	// Create a CLI app which takes a port option.
+	cli := huma.NewCLI(func(hooks huma.Hooks, options *Options) {
+		// Create a new router & API
+		router := chi.NewMux()
+		api = humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
+
+		// Register GET /greeting/{name}
+		huma.Register(api, huma.Operation{
+			OperationID: "get-greeting",
+			Summary:     "Get a greeting",
+			Method:      http.MethodGet,
+			Path:        "/greeting/{name}",
+		}, func(ctx context.Context, input *GreetingInput) (*GreetingOutput, error) {
+			resp := &GreetingOutput{}
+			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+			return resp, nil
+		})
+
+		// Tell the CLI how to start your router.
+		hooks.OnStart(func() {
+			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
+		})
 	})
 
-	// Start the server!
-	if err := http.ListenAndServe(":8888", router); err != nil {
-		log.Fatal(err)
-	}
+	// Add a command to print the OpenAPI spec.
+	cli.Root().AddCommand(&cobra.Command{
+		Use:   "openapi",
+		Short: "Print the OpenAPI spec",
+		Run: func(cmd *cobra.Command, args []string) {
+			b, _ := yaml.Marshal(api.OpenAPI())
+			fmt.Println(string(b))
+		},
+	})
+
+	// Run the CLI. When passed no commands, it starts the server.
+	cli.Run()
 }
